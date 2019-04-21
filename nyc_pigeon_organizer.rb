890e3be601ff2ba9db15e9eb3e92eeb5b35da9a9
Henry Koehler <henryk1229@gmail.com>
2019-04-21 19:10:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,10 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, attribute_hash|
     attribute_hash.each do |specific_key, specific_value|
       specific_value.each do |value|
-        new_hash[value]
+        if new_hash[value]
+          new_hash[value][attribute_hash]
+        else
+          new_hash[value]
       
     end
   end
